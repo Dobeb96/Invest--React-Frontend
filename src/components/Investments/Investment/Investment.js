858c60ef4dc977ca00677625a2619b6ea98d29cb
Dobeb96 <dobosz.ma@interia.pl>
2019-04-11 20:26:46 +0200

    Successful Investment creation

--- src/components/Investments/Investment/Investment.js
+++ src/components/Investments/Investment/Investment.js
@@ -3,32 +3,51 @@ import React, { Component } from 'react';
 import { Button, Col, Form, Jumbotron } from 'react-bootstrap';
 
 class Investment extends Component {
+  createInvestment(event) {
+    event.preventDefault();
+    const dataForm = new FormData(event.target);
+
+    let data = {}
+    for (var [key, value] of dataForm.entries()) {
+      data[key] = value
+    }
+
+    fetch('http://localhost:3000/investments', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+  }
+
   render() {
     return (
       <Jumbotron id="Investment">
-        <Form.Group controlId="newInvestmentForm">
+        <form onSubmit={this.createInvestment}>
           <Form.Row>
             <Col>
               <Form.Label>Invested</Form.Label>
-              <Form.Control type="number" placeholder="1000.0" />
+              <Form.Control name="invested" type="number" placeholder="1000.0" />
             </Col>
             <Col>
               <Form.Label>Earned</Form.Label>
-              <Form.Control type="number" placeholder="1000.0" />
+              <Form.Control name="earned" type="number" placeholder="1000.0" />
             </Col>
             <Col xs="8">
               <Form.Label>Notes</Form.Label>
-              <Form.Control type="text" placeholder="Notes" />
+              <Form.Control name="notes" type="text" placeholder="Notes" />
             </Col>
           </Form.Row>
           <Form.Row>
             <Col>
               <Button className="mt-1" variant="primary" type="submit">
-                Submit
+                Add
               </Button>
             </Col>
           </Form.Row>
-        </Form.Group>
+        </form>
       </Jumbotron>
     );
   }
