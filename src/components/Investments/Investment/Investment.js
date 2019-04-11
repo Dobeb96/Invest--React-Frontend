620c3b98d21c8998bc25eeea46f71e31eddf77e3
Dobeb96 <dobosz.ma@interia.pl>
2019-04-11 20:44:52 +0200

    Move creation of Investment to NewInvestment controller

--- src/components/Investments/Investment/Investment.js
+++ src/components/Investments/Investment/Investment.js
@@ -1,54 +1,9 @@
 import React, { Component } from 'react';
 
-import { Button, Col, Form, Jumbotron } from 'react-bootstrap';
-
 class Investment extends Component {
-  createInvestment(event) {
-    event.preventDefault();
-    const dataForm = new FormData(event.target);
-
-    let data = {}
-    for (var [key, value] of dataForm.entries()) {
-      data[key] = value
-    }
-
-    fetch('http://localhost:3000/investments', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data)
-    })
-  }
-
   render() {
     return (
-      <Jumbotron id="Investment">
-        <form onSubmit={this.createInvestment}>
-          <Form.Row>
-            <Col>
-              <Form.Label>Invested</Form.Label>
-              <Form.Control name="invested" type="number" placeholder="1000.0" />
-            </Col>
-            <Col>
-              <Form.Label>Earned</Form.Label>
-              <Form.Control name="earned" type="number" placeholder="1000.0" />
-            </Col>
-            <Col xs="8">
-              <Form.Label>Notes</Form.Label>
-              <Form.Control name="notes" type="text" placeholder="Notes" />
-            </Col>
-          </Form.Row>
-          <Form.Row>
-            <Col>
-              <Button className="mt-1" variant="primary" type="submit">
-                Add
-              </Button>
-            </Col>
-          </Form.Row>
-        </form>
-      </Jumbotron>
+
     );
   }
 }
--- src/components/Investments/NewInvestment/NewInvestment.js
+++ src/components/Investments/NewInvestment/NewInvestment.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import Investment from '../Investment/Investment';
 
-import { Jumbotron } from 'react-bootstrap';
+import { Button, Col, Form, Jumbotron } from 'react-bootstrap';
 
 class NewInvestment extends Component {
   constructor(props) {
@@ -9,21 +8,68 @@ class NewInvestment extends Component {
     this.state = {
       showNewForm: false
     };
-    this.showForm = this.showForm.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.createInvestment = this.createInvestment.bind(this);
   }
 
-  showForm() {
+  toggleForm() {
     this.setState(state => ({
-      showNewForm: true
+      showNewForm: !this.state.showNewForm
     }));
+  }
+
+  createInvestment(event) {
+    event.preventDefault();
+    const dataForm = new FormData(event.target);
+
+    let data = {}
+    for (var [key, value] of dataForm.entries()) {
+      data[key] = value
+    }
+
+    fetch('http://localhost:3000/investments', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+    this.toggleForm();
   }
 
   newInvestment() {
     if (this.state.showNewForm) {
-      return <Investment />
+      return (
+        <Jumbotron id="Investment">
+          <form onSubmit={this.createInvestment}>
+            <Form.Row>
+              <Col>
+                <Form.Label>Invested</Form.Label>
+                <Form.Control name="invested" type="number" placeholder="1000.0" />
+              </Col>
+              <Col>
+                <Form.Label>Earned</Form.Label>
+                <Form.Control name="earned" type="number" placeholder="1000.0" />
+              </Col>
+              <Col xs="8">
+                <Form.Label>Notes</Form.Label>
+                <Form.Control name="notes" type="text" placeholder="Notes" />
+              </Col>
+            </Form.Row>
+            <Form.Row>
+              <Col>
+                <Button className="mt-1" variant="primary" type="submit">
+                  Add
+                </Button>
+              </Col>
+            </Form.Row>
+          </form>
+        </Jumbotron>
+      );
     } else {
       return (
-        <Jumbotron id="NewInvestment" onClick={this.showForm}>
+        <Jumbotron id="NewInvestment" onClick={this.toggleForm}>
           <span className="NewRecord">+ Add record</span>
         </Jumbotron>
       )
